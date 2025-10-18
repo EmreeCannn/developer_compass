c5657241438bee2a636885d2a673d4c876e3c2ee
EmreeCannn <emrecangozleyici@gmail.com>
2025-10-19 02:08:43 +0300

    Fix: replaced text-white-500

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ function App() {
   strokeWidth="2"
   strokeLinecap="round"
   strokeLinejoin="round"
-  className="lucide lucide-lightbulb-icon w-8 h-8 text-yellow-400"
+  className="lucide lucide-lightbulb-icon w-8 h-8 text-white"
 >
   <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
   <path d="M9 18h6" />
